af77706facaa10317f10f6b8ad25b33301d45a5e
Daniil_Sanets <daniilsanets@gmail.com>
2025-10-31 16:42:11 +0300

    Add a docker-compose and liquibase.properties

--- src/main/java/sanets/dev/animechallenges/AnimeChallengesApplication.java
+++ src/main/java/sanets/dev/animechallenges/AnimeChallengesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnimeChallengesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeChallengesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeChallengesApplication.class, args);
+    }
 
 }
--- src/test/java/sanets/dev/animechallenges/AnimeChallengesApplicationTests.java
+++ src/test/java/sanets/dev/animechallenges/AnimeChallengesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AnimeChallengesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
