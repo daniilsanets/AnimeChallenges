524141975ff3277d4170ea37bfad1fe46af86d8f
Daniil_Sanets <daniilsanets@gmail.com>
2025-10-31 14:41:06 +0300

    first commiy

---
+++ src/main/java/sanets/dev/animechallenges/AnimeChallengesApplication.java
@@ -0,0 +1,13 @@
+package sanets.dev.animechallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sanets/dev/animechallenges/AnimeChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package sanets.dev.animechallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
