efb228c0854657c2b7da18529dcb8c2b24b5de4e
Daniil_Sanets <daniilsanets@gmail.com>
2025-11-07 14:01:41 +0300

    Added not null's constraits on max_attempts and score fields

--- src/main/resources/db/changelog/sql/006-create-quest-participation-table.sql
+++ src/main/resources/db/changelog/sql/006-create-quest-participation-table.sql
@@ -13,7 +13,7 @@ CREATE TABLE quest_participation (
                                      quest_uid UUID NOT NULL,
                                      quest_status quests_status NOT NULL,
                                      started_at TIMESTAMPTZ NOT NULL,
-                                     score INT NULL,
+                                     score INT NOT NULL,
                                      created_at TIMESTAMPTZ DEFAULT NOW(),
                                      updated_at TIMESTAMPTZ DEFAULT NOW()
 );
